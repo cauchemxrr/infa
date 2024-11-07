0e95850ceee02a62fc896ca8940a9bb32dde38dd
student-tets <guess.the.student@gmail.com>
2024-11-07 14:45:05 +0300

    переименовал файлы

--- playlist.py
+++
@@ -1,7 +0,0 @@
-def process_playlist(playlist):
-    if playlist == 'плейлист_1':
-        return 'Результат обработки плейлиста 1'
-    elif playlist == 'плейлист_2': 
-        return 'Результат обработки плейлиста 2'
-    else:
-        raise ValueError('Неизвестный плейлист')
